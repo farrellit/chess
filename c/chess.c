5a348beaf38903a4e7cde29474d495275ccafee3
Dan Farrell <dan@farrellit.net>
2014-04-25 16:43:10 -0500

    saving place

--- c/chess.c
+++ c/chess.c
@@ -1,77 +1,80 @@
 
 #include <stdlib.h>
 
-typedef char piece;
-typedef char position;
+typedef unsigned char piece;
+typedef unsigned char position;
 typedef char* position_set;
 
 // return file.
-char fileof( position p ){
+unsigned char fileof( position p ){
     return ( 0xF0 & p) >> 4;
 }  
 // return rank.  
-char rankof( position p ){
+unsigned char rankof( position p ){
     return 0x0F & p;
 }
 
 #define EMPTY   0x00
 #define PAWN    0x01
-#define KNIGHT  0X02
-#define BISHOP  0X03
-#define ROOK    0X04
-#define QUEEN   0X05
-#define KING    0X06
+#define KNIGHT  0x02
+#define BISHOP  0x03
+#define ROOK    0x04
+#define QUEEN   0x05
+#define KING    0x06
 
 #define WHITE   0x00
 #define BLACK   0x08
 
-#define WHITE_CASTLABLE  0x01
-#define BLACK_CASTLABLE  0x02
+#define WHITE_SHORTCASTLE  1
+#define WHITE_LONGCASTLE   2
+#define BLACK_SHORTCASTLE  4
+#define BLACK_LONGCASTLE   8
 
-typedef char boardmeta;
+typedef unsigned char boardmeta;
 
 typedef struct {    
-    char squares[32];
+    unsigned char squares[32];
     boardmeta meta;
 } board;
+
 
 #define EMPTY_BOARD 0x00000000000000000000000000000000;
 
 board* gamestart_board() {
     board* b = (board*)malloc(sizeof(board));
-    b->meta = WHITE_CASTLEABLE & BLACK_CASTLEABLE;
-    b->squares = { 
-        (( WHITE & ROOK   ) << 4 ) & ( WHITE & KNIGHT ), 
-        (( WHITE & BISHOP ) << 4 ) & ( WHITE & QUEEN  ), 
-        (( WHITE & KING   ) << 4 ) & ( WHITE & BISHOP ), 
-        (( WHITE & KNIGHT ) << 4 ) & ( WHITE & ROOK   ), 
-        (( WHITE & PAWN   ) << 4 ) & ( WHITE & PAWN   ),
-        (( WHITE & PAWN   ) << 4 ) & ( WHITE & PAWN   ),
-        (( WHITE & PAWN   ) << 4 ) & ( WHITE & PAWN   ),
-        (( WHITE & PAWN   ) << 4 ) & ( WHITE & PAWN   ),
-        0x00, 0x00, 0x00, 0x00, // empty row
-        0x00, 0x00, 0x00, 0x00, // empty row
-        0x00, 0x00, 0x00, 0x00, // empty row
-        0x00, 0x00, 0x00, 0x00, // empty row
-        (( WHITE & PAWN   ) << 4 ) & ( WHITE & PAWN   ),
-        (( WHITE & PAWN   ) << 4 ) & ( WHITE & PAWN   ),
-        (( WHITE & PAWN   ) << 4 ) & ( WHITE & PAWN   ),
-        (( WHITE & PAWN   ) << 4 ) & ( WHITE & PAWN   ),
-        (( WHITE & ROOK   ) << 4 ) & ( WHITE & KNIGHT ), 
-        (( WHITE & BISHOP ) << 4 ) & ( WHITE & QUEEN  ), 
-        (( WHITE & KING   ) << 4 ) & ( WHITE & BISHOP ), 
-        (( WHITE & KNIGHT ) << 4 ) & ( WHITE & ROOK   ) 
-    };
-    return b;
+    b->meta = 0xff; // all castling
+	 fprintf(stderr, "\033[43;30m%32s\033[0m\n", b->squares);
+    b->squares[0] = (( WHITE | ROOK   ) << 4 ) | ( WHITE | KNIGHT ); 
+    b->squares[1] = (( WHITE | BISHOP ) << 4 ) | ( WHITE | QUEEN  ); 
+    b->squares[2] = (( WHITE | KING   ) << 4 ) | ( WHITE | BISHOP ); 
+    b->squares[3] = (( WHITE | KNIGHT ) << 4 ) | ( WHITE | ROOK   );
+    b->squares[4] = (( WHITE | PAWN   ) << 4 ) | ( WHITE | PAWN   );
+    b->squares[5] = (( WHITE | PAWN   ) << 4 ) | ( WHITE | PAWN   );
+    b->squares[6] = (( WHITE | PAWN   ) << 4 ) | ( WHITE | PAWN   );
+    b->squares[7] = (( WHITE | PAWN   ) << 4 ) | ( WHITE | PAWN   );
+	 for( int i = 8; i < 24 ;i++ ) b->squares[i] = 0x00;
+    b->squares[24] = (( BLACK | PAWN   ) << 4 ) | ( BLACK | PAWN   );
+    b->squares[25] = (( BLACK | PAWN   ) << 4 ) | ( BLACK | PAWN   );
+    b->squares[26] = (( BLACK | PAWN   ) << 4 ) | ( BLACK | PAWN   );
+    b->squares[27] = (( BLACK | PAWN   ) << 4 ) | ( BLACK | PAWN   );
+    b->squares[28] = (( BLACK | ROOK   ) << 4 ) | ( BLACK | KNIGHT ); 
+    b->squares[29] = (( BLACK | BISHOP ) << 4 ) | ( BLACK | QUEEN  ); 
+    b->squares[30] = (( BLACK | KING   ) << 4 ) | ( BLACK | BISHOP ); 
+    b->squares[31] = (( BLACK | KNIGHT ) << 4 ) | ( BLACK | ROOK   );
+	 for( int i = 0; i < 32; i++ ){
+		fprintf(stderr, "0x%02x 0x%02x ", (b->squares[i] >> 4) & 15, b->squares[i] & 15 );
+		if( (i + 1) % 4 == 0) fprintf(stderr, "\n" );
+	 }
+   return b;
 }
-
 
 piece piece_at( const board* b, position p ){
-    return ( 
-      *(b->squares) >>  
-        ( ( fileof( p ) * 4 ) + 
-          ( rankof( p ) * 8 )  )  
-        ) & 0X0F;
+    unsigned char offset_bits = ( (rankof(p) * 32) + (fileof(p) * 4) ) ;
+	 unsigned char offset = offset_bits / 8;
+	 unsigned char shift  = 4 - (offset_bits % 8 );
+	 unsigned char pieceat = ( b->squares[offset ] >> shift ) & 15;
+	 //fprintf(stderr, "Piece at position %02x is at bit %u, offset %u, shift %u, and is 0x%x\n", 
+	// 	p, offset_bits, offset, shift, pieceat);
+	return pieceat;
 }
-
 
--- c/tests/chess.c
+++ c/tests/chess.c
@@ -1,27 +1,106 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 #include "../chess.c"
 
 char* showpos( char* s, position p ){
-    sprintf( s, "%c%u", fileof(p) + 'a', rankof(p) + 1 );
+    snprintf( s, 3, "%c%u", fileof(p) + 'a', rankof(p) + 1 );
     return s;
+}
+
+char* showpiece(char* buf, size_t bufsize, piece p ){
+	char* color;
+	if( ( p & 8 ) == BLACK )
+		color = "\033[30m";
+	else
+		color = "\033[37m";
+	char* pstr;
+	switch( p & 0x07 ){
+		case PAWN:
+			pstr = "Pa";
+			break;
+		case KNIGHT:
+			pstr = "Kn";
+			break;
+		case BISHOP:
+			pstr = "Bi";
+			break;
+		case ROOK:
+			pstr = "Ro";
+			break;
+		case QUEEN:
+			pstr = "Qu";
+			break;
+		case KING:
+			pstr = "Kg";
+			break;
+		default:
+		case EMPTY:
+			pstr = "  ";
+			break;
+	}
+	snprintf( buf, bufsize, "%s%s", color, pstr);
+	return buf;
+}
+
+#define BG_BLACK "\033[41m"
+#define BG_WHITE "\033[42m"
+#define PIECEBUF 16
+void showboard( board* b, char orientation ){
+	position p;
+	char* bg;
+	char piecebuf[PIECEBUF];
+	for( int rank = (orientation==WHITE?7:0); 
+		(orientation==WHITE?rank >= 0 : rank <= 7); 
+		( orientation ==WHITE ? rank-- : rank++) 
+	){
+		for( int file = 0; file < 8; file++ ){
+			p = ( file << 4 ) + rank;
+			//fprintf(stderr, "Printing piece at %s ( 0x%x ) \n", showpos( piecebuf, p ), piece_at(b,p ) );
+			if(  
+				( file % 2 == 0 && rank % 2 == 0 ) ||
+				( file % 2 == 1 && rank % 2 == 1 ) 
+			){
+				bg = BG_BLACK;
+			}else{
+				bg = BG_WHITE;
+			}
+			printf( "%s%s\033[0m", bg, showpiece( piecebuf, PIECEBUF, piece_at( b, p ) ) );
+		}
+		printf("\n");
+	}
 }
 
 int main(){
     char pos[3];
     char* strings[] = {
-        "h8"
+        "h8", "a1", "d6"
     };
     char positions[] = { 
-        0x3f
+        0x77, 0x00, 0x35
     };
-    for( int i = 0; i < sizeof( strings ) + 1; i++ ){
+    for( int i = 0; i < sizeof( positions ) ; i++ ){
+        printf("\033[33mTest: \033[0m 0x%02x should equal %s | ", 
+           	positions[i],
+            strings[i]
+        );
+	
         if( 0 != strncmp( strings[i], showpos( pos, positions[i]) , 2) ){
-            printf("\033[41mError!\033[0m 0x%02x should have equaled %s", 
-                positions+i,
-                strings[i]
+            printf("\033[41mError!\033[0m 0x%02x should have equaled %s, but came out as %s\n", 
+                positions[i],
+                strings[i],
+					 pos
             );
         }
+		  else {
+				printf( "\033[1;32mOK\033[0m\n" );
+		  }
     }
-
+	 board* b = gamestart_board();
+	 printf("From white's side:\n");
+	 showboard( b, WHITE);
+	 printf("\n");
+	 printf("From black's side:\n");
+	 showboard( b, BLACK);
 }
